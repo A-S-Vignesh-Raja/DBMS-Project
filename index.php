e376a2532ca103957ed6695fdefd2a528fc236ef
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-04-19 23:19:29 +0530

    added

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
         <div>
             <button type="submit"
                 class="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+                
                 Register
             </button>
         </div>
